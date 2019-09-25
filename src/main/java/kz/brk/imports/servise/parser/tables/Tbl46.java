35b492f126d4c202a67afed7a53bf79b7b67f79e
Азамат Айтказин <a.aitkazin@gmail.com>
2019-09-25 16:10:57 +0600

    int to bigDecimal

--- src/main/java/kz/brk/imports/servise/parser/tables/Tbl46.java
+++ src/main/java/kz/brk/imports/servise/parser/tables/Tbl46.java
@@ -18,13 +18,13 @@ public class Tbl46 {
         item.setGod(Calendar.getInstance().get(Calendar.YEAR));
         item.setIdSubclass(CheckInt.isNumeric(row.getCell(1).getStringCellValue())?row.getCell(1).getStringCellValue():"0");
 
-        item.setFld245AktvInostrVlt(CheckInt.cellToInt(row.getCell(247)));
-        item.setFld246KrtAktvInostrVlt(CheckInt.cellToInt(row.getCell(248)));
-        item.setFld247DlgAktvInostrVlt(CheckInt.cellToInt(row.getCell(249)));
-        item.setFld248ObzInostrVlt(CheckInt.cellToInt(row.getCell(250)));
-        item.setFld249KrtObzInostrVlt(CheckInt.cellToInt(row.getCell(251)));
-        item.setFld250DlgObzInostrVlt(CheckInt.cellToInt(row.getCell(252)));
-        item.setFld251ChstPzcInostrVlt(CheckInt.cellToInt(row.getCell(253)));
+        item.setFld245AktvInostrVlt(CheckInt.cellToBigDecimal(row.getCell(247)));
+        item.setFld246KrtAktvInostrVlt(CheckInt.cellToBigDecimal(row.getCell(248)));
+        item.setFld247DlgAktvInostrVlt(CheckInt.cellToBigDecimal(row.getCell(249)));
+        item.setFld248ObzInostrVlt(CheckInt.cellToBigDecimal(row.getCell(250)));
+        item.setFld249KrtObzInostrVlt(CheckInt.cellToBigDecimal(row.getCell(251)));
+        item.setFld250DlgObzInostrVlt(CheckInt.cellToBigDecimal(row.getCell(252)));
+        item.setFld251ChstPzcInostrVlt(CheckInt.cellToBigDecimal(row.getCell(253)));
 
         em.persist(item);
         em.flush();
